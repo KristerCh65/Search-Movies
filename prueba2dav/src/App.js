b533b7d3b82dc55bd296a770efaef5e2ad871408
KRISTER BRITHANNY MUNGUIA CHAVEZ <brithannychavez@unitec.edu>
2021-06-13 15:59:31 -0600

    creating proyect crud prueba on react

---
+++ prueba2dav/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Header } from './Componets/Header';
+import { ContainerMain } from './Componets/Container';
+
+function App() {
+  return (
+    <div className="App">
+      <Header/>
+      <ContainerMain/>
+    </div>
+  );
+}
+
+export default App;
